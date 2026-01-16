b4b823c8ad30efc7648e4720df88811afdf8fa4b
Julia Tomczak <komorczak213769@gmail.com>
2026-01-16 13:09:33 +0100

    dodane koło

--- prostokat.c
+++ prostokat.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
-float area_rectangle(float a, float b) {
-	return a * b;
+float area_circle(float r) {
+    return 3.14159 * r * r;
 }
+//float area_rectangle(float a, float b) {
+//     return a * b;
+//}
 
-void calculate_area_rectangle() {
-	float a, b;
+void calculate_area_circle() {
+    float r;
 
-	printf("Podaj dlugosc boku a: ");
-	scanf("%f", &a);
+    printf("Podaj promien kola: ");
+    scanf("%f", &r);
 
-	printf("Podaj dlugosc boku b: ");
-	scanf("%f", &b);
-
-	printf("Pole prostokata: %f\n", area_rectangle(a, b));
+    printf("Pole kola: %f\n", area_circle(r));
 }
+//void calculate_area_rectangle() {
+    //float a, b;
+
+    //printf("Podaj dlugosc boku a: ");
+    //scanf("%f", &a);
+    //printf("Podaj dlugosc boku b: ");
+    //scanf("%f", &b);
+
+    //printf("Pole prostokata: %f\n", area_rectangle(a, b));
+//}
 
 int main() {
-	calculate_area_rectangle();
-
-	return 0;
+    calculate_area_circle();
+    //calculate_area_rectangle()
+    return 0;
 }
+
