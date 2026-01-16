3a62e6def0b1013f3c69ed4c317e7791abcbe2d9
Zuzanna Jerz <22597@student.konin.edu.pl>
2026-01-16 13:14:27 +0100

    dodano menu

--- prostokat.c
+++ prostokat.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+
 float area_circle(float r) {
-    return 3.14159 * r * r;
+    return 3.14159f * r * r;
 }
-//float area_rectangle(float a, float b) {
-//     return a * b;
-//}
+
+float area_rectangle(float a, float b) {
+    return a * b;
+}
+
 
 void calculate_area_circle() {
     float r;
@@ -13,22 +16,41 @@ void calculate_area_circle() {
     printf("Podaj promien kola: ");
     scanf("%f", &r);
 
-    printf("Pole kola: %f\n", area_circle(r));
+    printf("Pole kola: %.2f\n", area_circle(r));
 }
-//void calculate_area_rectangle() {
-    //float a, b;
 
-    //printf("Podaj dlugosc boku a: ");
-    //scanf("%f", &a);
-    //printf("Podaj dlugosc boku b: ");
-    //scanf("%f", &b);
+void calculate_area_rectangle() {
+    float a, b;
 
-    //printf("Pole prostokata: %f\n", area_rectangle(a, b));
-//}
+    printf("Podaj dlugosc boku a: ");
+    scanf("%f", &a);
+
+    printf("Podaj dlugosc boku b: ");
+    scanf("%f", &b);
+
+    printf("Pole prostokata: %.2f\n", area_rectangle(a, b));
+}
 
 int main() {
-    calculate_area_circle();
-    //calculate_area_rectangle()
+    int choice;
+
+    printf("=== MENU ===\n");
+    printf("1. Pole kola\n");
+    printf("2. Pole prostokata\n");
+    printf("Wybierz opcje: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            calculate_area_circle();
+            break;
+        case 2:
+            calculate_area_rectangle();
+            break;
+        default:
+            printf("Nieprawidlowa opcja!\n");
+    }
+
     return 0;
 }
 
